f728b58e744b50ba2392bb36cac65e571677733c
Arthi Halder <159602371+bsastudio601@users.noreply.github.com>
2026-06-18 18:47:35 +0600

    Add files via upload

---
+++ Firmware/QMK/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define ENCODER_RESOLUTION 4
+
+#define RGBLIGHT_LIMIT_VAL 120
+
+#define DEBOUNCE 5
+#define WS2812_DI_PIN GP7
---
+++ Firmware/QMK/rules.mk
@@ -0,0 +1,2 @@
+ENCODER_MAP_ENABLE = yes
+WS2812_DRIVER = vendor
